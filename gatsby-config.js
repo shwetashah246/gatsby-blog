20c15dc986cf1cd99240e89a7fde6126a6777f42
Shweta Shah <shweta.shah246@gmail.com>
2022-01-05 23:56:34 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,16 @@
 module.exports = {
-    siteMetadata: {
-      title: "My First Gatsby Site",
+  siteMetadata: {
+    title: "My First Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
     },
-    plugins: [
-      "gatsby-plugin-image",
-      "gatsby-plugin-sharp",
-    ],
+  ],
 };
